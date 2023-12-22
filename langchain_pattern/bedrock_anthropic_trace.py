2a7754c7b8db9a99867e45f1de4fced972669cfa
Hunter Sneed <hello@huntersneed.com>
2023-12-22 10:43:34 -0600

    updated for guardian poc

--- langchain_pattern/bedrock_anthropic_trace.py
+++ langchain_pattern/bedrock_anthropic_trace.py
@@ -1,7 +1,6 @@
 import os
-from langchain.llms import OpenAI
 from langchain.chains import LLMChain
-from langchain.chat_models import ChatAnthropic, ChatOpenAI
+from langchain.chat_models import ChatAnthropic
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema.output_parser import StrOutputParser
 from langfuse.callback import CallbackHandler
@@ -9,17 +8,14 @@ from langchain_community.llms.bedrock import (
     Bedrock,
     BedrockBase,
 )
-import boto3
-from langchain.chains import LLMChain
 
 # Load API keys from environment variables
-PUBLIC_KEY = "pk-lf-22847867-6b8f-4c5e-84c6-3d1370320f63" # these are not sensative keys
-SECRET_KEY = "sk-lf-8c0c33e7-3008-4b16-bd51-8b0dedbe6c61" # these are not sensative keys
-LANGFUSE_HOST = "http://192.168.1.134:3000/" # these are not sensative keys
-OPENAI_API_KEY = os.getenv('WORDPRESS_PERSONAL_OPENAI_API')
+PUBLIC_KEY = os.getenv('LANGFUSE_PUBLIC_KEY')
+SECRET_KEY = os.getenv('LANGFUSE_SECRET_KEY')
+LANGFUSE_HOST =  os.getenv('LANGFUSE_HOST')
 
 # Check if keys are set
-if not all([PUBLIC_KEY, SECRET_KEY, LANGFUSE_HOST, OPENAI_API_KEY]):
+if not all([PUBLIC_KEY, SECRET_KEY, LANGFUSE_HOST]):
     raise ValueError("API keys are not set in environment variables")
 
 handler = CallbackHandler(PUBLIC_KEY, SECRET_KEY, LANGFUSE_HOST)
